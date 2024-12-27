d975ef63d1f163bf3d83ab80d763370c34d8b3c2
Marko Man <darkobas@gmail.com>
2024-12-27 17:23:51 +0530

    vendor: Add black theme for dark mode [3/3]
    Change-Id: I6bfa5a8d1a1ae31444acbbf60da942c4961d2171
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Signed-off-by: Jesse Chan <jc@lineageos.org>
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -254,6 +254,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/flare/overlay/common
 
 PRODUCT_PACKAGES += \
+    AndroidBlackThemeOverlay \
     CustomFontPixelLauncherOverlay \
     DocumentsUIOverlay \
     NetworkStackOverlay \
