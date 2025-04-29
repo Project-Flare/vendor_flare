a70d61b9106bd0bbe01802e5973667678a68eaca
cjh1249131356 <cjh1249131356@gmail.com>
2025-04-29 17:58:59 +0000

    soong: Add TARGET_USES_OPLUS_CAMERA
    Change-Id: Ifb3ff3a1141712a255640676d24b018ae516add8
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -38,7 +38,8 @@ SOONG_CONFIG_flareGlobalVars += \
     target_trust_usb_control_path \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
-    uses_legacy_fd_fbdev
+    uses_legacy_fd_fbdev \
+    uses_oplus_camera
 
 # Soong bool variables
 SOONG_CONFIG_flareGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
@@ -64,6 +65,7 @@ SOONG_CONFIG_flareGlobalVars_target_trust_usb_control_disable := $(TARGET_TRUST_
 SOONG_CONFIG_flareGlobalVars_disable_bluetooth_le_read_buffer_size_v2 := $(TARGET_DISABLE_BLUETOOTH_LE_READ_BUFFER_SIZE_V2)
 SOONG_CONFIG_flareGlobalVars_disable_bluetooth_le_set_host_feature := $(TARGET_DISABLE_BLUETOOTH_LE_SET_HOST_FEATURE)
 SOONG_CONFIG_flareGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
+SOONG_CONFIG_flareGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 
 # Lineage Health HAL
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)
