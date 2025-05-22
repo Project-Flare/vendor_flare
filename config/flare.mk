3d8e892874e01f9eabafc53702542bda0bd1bd7c
Joey Huab <jhuab@edukasyon.ph>
2025-05-22 13:12:04 +0900

    config: Add RELEASE and SDK_INT pihooks props

--- config/flare.mk
+++ config/flare.mk
@@ -5,8 +5,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_PRODUCT?=cheetah_beta \
     persist.sys.pihooks_DEVICE?=cheetah \
     persist.sys.pihooks_ID?=BP31.250502.008 \
+    persist.sys.pihooks_RELEASE?=12 \
     persist.sys.pihooks_SECURITY_PATCH?=2025-05-05 \
-    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SDK_INT?=32
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PihooksGmsFp="google/cheetah_beta/cheetah:16/BP31.250502.008/13497110:user/release-keys" \
